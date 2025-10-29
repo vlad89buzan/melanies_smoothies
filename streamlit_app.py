41aa22eb88469885e5c197eb4a50eb879287a50d
Vladyslav Buzan <99654057+vlad89buzan@users.noreply.github.com>
2025-10-29 12:52:58 +0200

    Update API endpoint to use search_on value

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if ingredients_list:
         st.write('The search value for ', fruit,' is ', search_on, '.')
         
         st.subheader(fruit+ ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
         st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     st.write(ingredients_string)
 
